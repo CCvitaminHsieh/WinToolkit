4daf5c43dfb4c2e3d1fcda2139616c509cc09b89
Jimste <37689294+CCvitaminHsieh@users.noreply.github.com>
2022-09-04 03:02:17 +0800

    Update data_backup.bat

--- data_backup.bat
+++ data_backup.bat
@@ -5,27 +5,30 @@ set destination_dir=D:\LabberDataBackup\
 set zipname=TestArchive.zip
 set zip_flag=1==1
 set copy_flag=1==1
+set pause_flag=1==0
 
 echo %source_directory% -> %destination_dir%
 if exist %destination_dir% (
-    echo %destination_dir% exists
+	echo %destination_dir% exists
 ) else (
-    mkdir %destination_dir%
+	mkdir %destination_dir%
 )
 if %copy_flag% (
-    echo.
+	echo.
 	call robocopy %source_directory% %destination_dir% /E
 )
 if %zip_flag% (
-    set zippath=%destination_dir%%zipname%
+	set zippath=%destination_dir%%zipname%
 	echo Add new data to archive
 	echo ArchivePath: %zippath% 
 	echo ArchiveDstination: %destination_dir%
         cd %destination_dir%
 	if exist %zipname% (
-	    echo %zipname% exists in the directory.
+		echo %zipname% exists in the directory.
 	) else (
-	    call 7z a %zipname% %destination_dir% 
+		call 7z a %zipname% %destination_dir% 
 	)
 )
-pause
+if %cp_flag% (
+	pause
+)
